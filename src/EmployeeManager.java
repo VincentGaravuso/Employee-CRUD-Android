df7f7482c6c66e7836f7d8f4f2f2427105b8b462
Vinnyg96 <vinnyg96@homtmail.com>
2019-05-01 13:48:05 -0400

    Add employee to list functionality added.

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -1,5 +1,24 @@
 
-public class EmployeeManager
-{
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeManager{
+
+    List<Employee> empList = new ArrayList<>();
+
+    public boolean addEmployee(String fName, String lName, int ID, int year, int month, int day) {
+        if (duplicateCheck(ID) == false) {
+            return false;
+        }
+        Employee e = new Employee(fName, lName, ID, year, month, day);
+        empList.add(e);
+        return true;
+    }
+
+    private boolean duplicateCheck(int ID) {
+        for (Employee e : empList) 
+            if (ID == e.getID()) 
+                return false;
+        return true;
+    }
 }
